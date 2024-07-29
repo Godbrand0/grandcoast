782ca5cd95b8ca67ae99549f12fa6a40cfa52db8
Godbrand0 <140930314+Godbrand0@users.noreply.github.com>
2024-07-29 13:36:00 -0700

    updating big image when small image is clicked

--- grandcoast.js
+++ grandcoast.js
@@ -17,3 +17,13 @@ ScrollReveal().reveal(".header__container .btn", {
   ...scrollRevealOption,
   delay: 1000,
 });
+
+// function myFunction(smallImg) {
+//   let roomImg = document.querySelector(".room_img");
+//   roomImg.src = smallImg.src;
+// }
+function myFunction(li) {
+  let smallImg = li.querySelector("img");
+  let roomImg = document.querySelector(".room_img");
+  roomImg.src = smallImg.src;
+}
