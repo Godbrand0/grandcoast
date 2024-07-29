63d51f6915bc1c658ad43b6e8f936d6cd006f284
Godbrand0 <140930314+Godbrand0@users.noreply.github.com>
2024-07-29 09:51:04 -0700

    grandcoast-hotel-landing page

---
+++ grandcoast.js
@@ -0,0 +1,19 @@
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+//header container
+
+scrollReveal().reveal(".header_container h1", scrollRevealOption);
+
+ScrollReveal().reveal(".header__container h4", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__container .btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
