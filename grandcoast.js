040eebdfd6b3dfa418f72f04dfe1f9d4b05e899e
Godbrand0 <140930314+Godbrand0@users.noreply.github.com>
2024-07-31 21:48:09 -0700

    review-slider

--- grandcoast.js
+++ grandcoast.js
@@ -1,43 +1,10 @@
-const scrollRevealOption = {
-  distance: "50px",
-  origin: "bottom",
-  duration: 1000,
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const sliderTrack = document.querySelector(".slider-track");
+  const reviewBoxes = Array.from(sliderTrack.children);
 
-//header container
-
-scrollReveal().reveal(".header_container h1", scrollRevealOption);
-
-ScrollReveal().reveal(".header__container h4", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-
-ScrollReveal().reveal(".header__container .btn", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-
-// function myFunction(smallImg) {
-//   let roomImg = document.querySelector(".room_img");
-//   roomImg.src = smallImg.src;
-// }
-function myFunction(li) {
-  let smallImg = li.querySelector("img");
-  let roomImg = document.querySelector(".room_img");
-  roomImg.src = smallImg.src;
-}
-
-// const swiper = new Swiper(".swiper", {
-//   // Optional parameters
-//   direction: "vertical",
-//   loop: true,
-// });
-var swiper = new Swiper(".myswiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  // Duplicate the review boxes to create an infinite effect
+  reviewBoxes.forEach((box) => {
+    const clone = box.cloneNode(true);
+    sliderTrack.appendChild(clone);
+  });
 });
