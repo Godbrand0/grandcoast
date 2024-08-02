eb1bb8db92fcab718f94db7cb612df947180ec1a
Godbrand0 <140930314+Godbrand0@users.noreply.github.com>
2024-08-02 06:40:32 -0700

    hotel-information

--- grandcoast.js
+++ grandcoast.js
@@ -1,27 +1,3 @@
-const scrollRevealOption = {
-  distance: "50px",
-  origin: "bottom",
-  duration: 1000,
-};
-
-//header container
-
-scrollReveal().reveal(".header_container h1", scrollRevealOption);
-
-ScrollReveal().reveal(".header__container h4", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-
-ScrollReveal().reveal(".header__container .btn", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-
-// function myFunction(smallImg) {
-//   let roomImg = document.querySelector(".room_img");
-//   roomImg.src = smallImg.src;
-// }
 function myFunction(li) {
   let smallImg = li.querySelector("img");
   let roomImg = document.querySelector(".room_img");
