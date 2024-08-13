f6333266f5b2cf712734c81d1d05fea49e615f27
Godbrand0 <140930314+Godbrand0@users.noreply.github.com>
2024-08-12 19:43:22 -0700

    loader-animation

--- grandcoast.js
+++ grandcoast.js
@@ -1,3 +1,4 @@
+//reciews container
 function myFunction(li) {
   let smallImg = li.querySelector("img");
   let roomImg = document.querySelector(".room_img");
@@ -15,6 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// menu and sidebar
 const menu = document.querySelector(".menu");
 const close = document.querySelector(".close");
 const sidebar = document.querySelector(".side-bar");
@@ -26,49 +28,39 @@ close.addEventListener("click", () => {
   sidebar.classList.remove("active");
 });
 
-let heroData = [
-  {
-    text1: "enjoy your day",
-    text2: "elegant capturing",
-    text3: "experience it",
-    image:
-      "images/10816199_bee272a1bf134798be07b4215f872027_jpeg_jpege6851475d93476a9c34804a78a9dd435.jpg",
-  },
-  {
-    text1: "make a break",
-    text2: "adventure starts here",
-    text3: "book a stay",
-    image: "images/full-shot-man-carrying-baggage.jpg",
-  },
-  {
-    text1: "imperial serenity",
-    text2: "perfect blend of eastern and western culture",
-    text3: "explore more",
-    image: "images/front-view-glasses-beer-with-wheat.jpg",
-  },
-];
+//loader animation
+const loaderImg = document.querySelector(".loader_bg");
+setTimeout(function () {
+  loaderImg.classList.toggle("hidden");
+}, 3000);
 
+//header-box animation
 let boxes = document.querySelectorAll("header .header_texts");
 let currentIndex = 0;
-let intervalTime = 4000; // 2 seconds interval
+let intervalTime = 4000;
 
 function iterateBoxes() {
-  // Clear any previous active classes
   boxes.forEach((box) => box.classList.remove("true"));
 
-  // Add active class to the current box
   boxes[currentIndex].classList.add("true");
 
-  // Move to the next box
   currentIndex++;
 
-  // If we reach the end of the boxes, reset to the first one
   if (currentIndex >= boxes.length) {
     currentIndex = 0;
   }
 }
+const header = document.querySelector("header");
 
-// Start the iteration
+header.addEventListener("mouseover", () => {
+  header.style.animationPlayState = "paused";
+});
+
+header.addEventListener("mouseout", () => {
+  header.style.animationPlayState = "running";
+});
+
+// number counter
 setInterval(iterateBoxes, intervalTime);
 
 let valueDisplays = document.querySelectorAll(".num");
